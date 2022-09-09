5efeaa05ea79460c62c8028dd37d03b4e272df92
Valerio <canivalerio93@gmail.com>
2022-09-09 17:32:16 +0200

    fine esercizio più piccolo bonus aggiunto da me per l'ignoranza

--- receiver.php
+++ receiver.php
@@ -4,14 +4,20 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP Badwords</title>
+    <title>Document</title>
 </head>
 <body>
     <?php
-        $testo = $_POST['text'];
-        $testo_censurato = str_replace("foca", "f**a", $testo);
+        $parolaccia = $_POST['badword'];
+        $testo = "Al polo sud non ci sono ne una foche ne un cervi";
+        if($parolaccia=="foche"){
+            $censura = "f***e";
+        }elseif($parolaccia=="cervi"){
+            $censura = "c***i";
+        }
+        $testo_censurato = str_replace($parolaccia, $censura, $testo);
     ?>
-    
+    <p>il testo è lungo <?php echo  strlen($testo_censurato); ?> caratteri</p>
     <p><?php echo  $testo_censurato; ?></p>
 </body>
 </html>
